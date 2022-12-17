2c74933f8c7268a718d06860e45a67afe2fd15c9
DanilSPB <sherlokDP@gmail.com>
2022-12-17 19:52:45 +0300

    Added delete task functionality

--- Controllers/ToDoListController.cs
+++ Controllers/ToDoListController.cs
@@ -4,56 +4,55 @@ using ToDo_List.Models;
 
 namespace ToDo_List.Controllers
 {
-    public class ToDoListController : Controller
-    {
-        private readonly ApplicationDbContext _db;
-        public ToDoListController(ApplicationDbContext db)
-        {
-            _db = db;
-        }
-        public IActionResult Index()
-        {
-            IEnumerable<ToDoList> objCategoryList = _db.ToDoLists;
-            return View(objCategoryList);
-        }
-                                                                                    // Create actions
-        //GET
-        public IActionResult Create()
-        {
-            return View();
-        }
-        //POST
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create(ToDoList obj)
-        {
-            if(obj.DateStart >= obj.Deadline)
-            {
-                ModelState.AddModelError("deadline", "Deadline can not be before or equal date start");
-            }
-            if(ModelState.IsValid)
-            {
-                _db.ToDoLists.Add(obj);
-                _db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(obj);
-        }
+	public class ToDoListController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+		public ToDoListController(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+		public IActionResult Index()
+		{
+			IEnumerable<ToDoList> objCategoryList = _db.ToDoLists;
+			return View(objCategoryList);
+		}
+		// Create actions
+		//GET
+		public IActionResult Create()
+		{
+			return View();
+		}
+		//POST
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Create(ToDoList obj)
+		{
+			if (obj.DateStart >= obj.Deadline)
+			{
+				ModelState.AddModelError("deadline", "Deadline can not be before or equal date start");
+			}
+			if (ModelState.IsValid)
+			{
+				_db.ToDoLists.Add(obj);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View(obj);
+		}
 
-		                                                                // Edit acions
+		// Edit acions
 
 		public IActionResult Edit(int? id)
 		{
-            if(id == null || id == 0)
-            {
-                return NotFound();  
-            }
-
-            var taskFromDb = _db.ToDoLists.Find(id);
-            if (taskFromDb == null)
-            {
-                return NotFound();
-            }
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			var taskFromDb = _db.ToDoLists.Find(id);
+			if (taskFromDb == null)
+			{
+				return NotFound();
+			}
 			return View(taskFromDb);
 		}
 		//POST
@@ -72,6 +71,34 @@ namespace ToDo_List.Controllers
 				return RedirectToAction("Index");
 			}
 			return View(obj);
+		}
+
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			var taskFromDb = _db.ToDoLists.Find(id);
+			if (taskFromDb == null)
+			{
+				return NotFound();
+			}
+			return View(taskFromDb);
+		}
+		//POST
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult DeletePOST(int? id)
+		{
+			var taskFromDb = _db.ToDoLists.Find(id);
+			if (taskFromDb == null)
+			{
+				return NotFound();
+			}
+			_db.ToDoLists.Remove(taskFromDb);
+			_db.SaveChanges();
+			return RedirectToAction("Index");
 		}
 	}
 }
