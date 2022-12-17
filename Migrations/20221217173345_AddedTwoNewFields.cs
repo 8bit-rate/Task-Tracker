0df18591fe20e8a11893e9338c0cb4225bcabf99
DanilSPB <sherlokDP@gmail.com>
2022-12-17 20:35:51 +0300

    Added two new fields in model

---
+++ Migrations/20221217173345_AddedTwoNewFields.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDo_List.Migrations
+{
+    public partial class AddedTwoNewFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DeadLine",
+                table: "ToDoLists",
+                newName: "Deadline");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Importance",
+                table: "ToDoLists",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Note",
+                table: "ToDoLists",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Importance",
+                table: "ToDoLists");
+
+            migrationBuilder.DropColumn(
+                name: "Note",
+                table: "ToDoLists");
+
+            migrationBuilder.RenameColumn(
+                name: "Deadline",
+                table: "ToDoLists",
+                newName: "DeadLine");
+        }
+    }
+}
--- Models/ToDoList.cs
+++ Models/ToDoList.cs
@@ -11,8 +11,10 @@ namespace ToDo_List.Models
         [DisplayName("Task")]
         public string Content{ get; set; }
         public string Description { get; set; }
+        public string Note { get; set; }
         [Range(0, 2)]
         public TaskStatus Status { get; set; } = TaskStatus.Created;
+        public Importance Importance { get; set; } = Importance.Low;
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
         [Required]
         [DisplayName("Date start")]
