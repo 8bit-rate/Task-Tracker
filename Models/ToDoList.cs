056464b514fdba805a1d7b0f27815770b048a2a7
DanilSPB <sherlokDP@gmail.com>
2022-12-17 23:31:12 +0300

    little fix

--- Models/ToDoList.cs
+++ Models/ToDoList.cs
@@ -12,9 +12,9 @@ namespace ToDo_List.Models
         public string Content{ get; set; }
         public string? Description { get; set; }
         public string? Note { get; set; }
-        [Range(0, 2)]
+        
         public TaskStatus Status { get; set; } = TaskStatus.Created;
-		[Range(0, 2)]
+		
 		public Importance Importance { get; set; } = Importance.Low;
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
         [Required]
